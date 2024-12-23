af56c7abd1d387cc8bc8af42a77424408eb10ab4
Jun Song <87601811+syjn99@users.noreply.github.com>
2024-12-23 18:47:04 +0900

    feat: add tracing (#19)
    * feat: add tracing & match verbosity with log level
    
    * fix: make linter happy
    
    * fix: use env filter for logger

--- bin/ream/src/main.rs
+++ bin/ream/src/main.rs
@@ -1,13 +1,24 @@
 use clap::Parser;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 use ream::cli::{Cli, Commands};
 
 fn main() {
+    // Set the default log level to `info` if not set
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Node(cmd) => {
-            println!("Starting node with verbosity {}", cmd.verbosity);
+        Commands::Node(_cmd) => {
+            info!("Starting node");
         }
     }
 }
