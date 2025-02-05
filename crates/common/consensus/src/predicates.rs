ac11f0ded0ca8548cb598fb69fab0d0c530c359e
Varun Doshi <61531351+varun-doshi@users.noreply.github.com>
2025-02-05 20:06:37 +0530

    feat: implement process_attester_slashing (#57)

--- crates/common/consensus/src/predicates.rs
+++ crates/common/consensus/src/predicates.rs
@@ -1,7 +1,7 @@
 use crate::attestation_data::AttestationData;
 
 /// Check if ``data_1`` and ``data_2`` are slashable according to Casper FFG rules.
-pub fn is_slashable_attestation_data(data_1: AttestationData, data_2: AttestationData) -> bool {
+pub fn is_slashable_attestation_data(data_1: &AttestationData, data_2: &AttestationData) -> bool {
     (data_1 != data_2 && data_1.target.epoch == data_2.target.epoch)
         || (data_1.source.epoch < data_2.source.epoch && data_2.target.epoch < data_1.target.epoch)
 }
